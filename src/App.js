1c71a25ea68e6b9551129f4b48bc642b3903fb00
omegamaster86 <masya868686@gmail.com>
2023-05-24 17:21:16 +0900

    repair: style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Example from "../src/component/addTodo/index.jsx";
 
 const App = () => {
   return (
     <div className='App'> 
       <h1>Todo List</h1>
-      <h3>ADD TODO</h3>
+      <h2>ADD TODO</h2>
+      <Example />
     </div>
   );
-}
+};
 
 export default App;
