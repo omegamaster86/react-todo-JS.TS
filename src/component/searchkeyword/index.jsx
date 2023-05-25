98c7aa53914ca5092e8c71909d06a28020354877
omegamaster86 <masya868686@gmail.com>
2023-05-25 20:20:10 +0900

    input, style　追加

---
+++ src/component/searchkeyword/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const SearchKeyword = () => {
+    return (
+        <input className="SearchKeyword" type="text" value="SearchKeyword"/>
+    )
+};
+
+export default SearchKeyword;
