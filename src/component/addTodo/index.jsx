362958e2f4b269dc9dbe54e9ebd0c5bd493cbe7a
omegamaster86 <masya868686@gmail.com>
2023-05-27 19:04:10 +0900

    useState  追加

--- src/component/addTodo/index.jsx
+++ src/component/addTodo/index.jsx
@@ -1,8 +1,11 @@
 import "./style.css";
+import { useState } from "react";
 
 const AddTodo = () => {
+    const [text, setText] = useState("New Todo")
     return (
-        <input type="text" value="NewTodo"/>
+        <input type="text" value={text}
+         onChange={(e) => setText(e.target.value)}/>
     )
 };
 
