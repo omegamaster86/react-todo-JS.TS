8198893901b1623961c1a44efee248685c22da75
omegamaster86 <masya868686@gmail.com>
2023-05-27 20:20:10 +0900

    add: placeholder

--- src/component/addTodo/index.jsx
+++ src/component/addTodo/index.jsx
@@ -2,9 +2,10 @@ import "./style.css";
 import { useState } from "react";
 
 const AddTodo = () => {
-    const [text, setText] = useState("New Todo")
+    const [text, setText] = useState("")
     return (
         <input type="text" value={text}
+         placeholder={"New Todo"}
          onChange={(e) => setText(e.target.value)}/>
     )
 };
