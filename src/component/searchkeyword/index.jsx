6210785762371ef69b9a4f6c28d4e90e2e05726d
omegamaster86 <masya868686@gmail.com>
2023-05-28 19:04:50 +0900

    add: placeholder　

--- src/component/searchkeyword/index.jsx
+++ src/component/searchkeyword/index.jsx
@@ -1,8 +1,16 @@
 import "./style.css";
+import { useState } from "react";
 
 const SearchKeyword = () => {
+    const [text, setText] = useState("")
     return (
-        <input className="SearchKeyword" type="text" value="SearchKeyword"/>
+        <input 
+        className="SearchKeyword" 
+        type="text" 
+        value={text}
+        placeholder={"SearchKeyword"}
+        onChange={(e) => setText(e.target.value)}
+        />
     )
 };
 
