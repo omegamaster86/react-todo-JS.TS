6102d28994a5cb4d631762052442820b8c63bfe4
omegamaster86 <masya868686@gmail.com>
2023-05-28 18:40:55 +0900

    インデント 修正

--- src/component/addTodo/index.jsx
+++ src/component/addTodo/index.jsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 const AddTodo = () => {
     const [text, setText] = useState("")
     return (
-        <input type="text" value={text}
-         placeholder={"New Todo"}
-         onChange={(e) => setText(e.target.value)}/>
+        <input type="text" 
+        value={text}
+        placeholder={"New Todo"}
+        onChange={(e) => setText(e.target.value)}/>
     )
 };
 
