c0d0df6d0aed149d365e62b74287c3d00f64c074
omegamaster86 <masya868686@gmail.com>
2023-05-31 16:38:50 +0900

    init

---
+++ src/component/TodoTemplate/index.jsx
@@ -0,0 +1,7 @@
+import { InputForm } from "../InputForm/index";
+import { useState } from "react";
+import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../data";
+
+export const TodoTemplate = () => {
+    
+}
