b483266dda52214e3dc8bf67f4554c8823ec73c5
omegamaster86 <masya868686@gmail.com>
2023-05-31 17:11:38 +0900

    props 追加

--- src/component/InputForm/index.jsx
+++ src/component/InputForm/index.jsx
@@ -1,14 +1,15 @@
 import styles from  "./style.module.css";
 
 export const InputForm = (props) => {
-    const { placeholder, inputValue } = props;
+    const { placeholder, inputValue, KeyDown } = props;
     
     return (
         <input className={styles.input}
         type="text" 
         value={inputValue}
         placeholder={placeholder}
-        // onChange={(e) => setText(e.target.value)}
+        onKeyDown={KeyDown}
+        // onChange={handleChangeValue}
         />
     )
 };
