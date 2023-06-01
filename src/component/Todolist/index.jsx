d616ccd2206cf319094f065c283c44643ea8f5be
omegamaster86 <masya868686@gmail.com>
2023-06-01 17:10:38 +0900

    todolist init

--- src/component/Todolist/index.jsx
+++ src/component/Todolist/index.jsx
@@ -1,1 +1,21 @@
-import "./style.css";
+import styles from  "./style.module.css";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Todolist = (props) => {
+    const { todoList, deleteTodo } = props;
+
+    return (
+        <ul>
+            <li>
+                <div className={styles.far}>
+                    <FontAwesomeIcon
+                    icon={faTrashAlt}
+                    size="lg"
+                    onClick={() => deleteTodo(todo.id, todo.title)}
+                    />
+                </div>
+            </li>
+        </ul>
+    );
+}
