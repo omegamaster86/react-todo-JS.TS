8932a64911a37b47752d3362d08d469dd7de3a76
omegamaster86 <masya868686@gmail.com>
2023-06-02 00:58:18 +0900

    h1  削除

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { TodoTemplate } from './component/TodoTemplate/index';
 const App = () => {
   return (
     <div className='App'> 
-      <h1>Todo List</h1>
       <TodoTemplate />
     </div>
   );
