f1079cfa704438c41321430ef0d5c33d6466a4ff
omegamaster86 <masya868686@gmail.com>
2023-06-02 15:23:09 +0900

    props 変更

--- src/component/InputForm/index.jsx
+++ src/component/InputForm/index.jsx
@@ -1,14 +1,14 @@
 import styles from  "./style.module.css";
 
 export const InputForm = (props) => {
-    const { placeholder, inputValue, KeyDown, changeValue } = props;
+    const { placeholder, inputValue, enteredTodo, changeValue } = props;
     
     return (
         <input className={styles.input}
         type="text" 
         value={inputValue}
         placeholder={placeholder}
-        onKeyDown={KeyDown}
+        onKeyDown={enteredTodo}
         onChange={changeValue}
         />
     );
