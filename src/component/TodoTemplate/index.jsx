4aa39f6ae235bdd1670d7f24fe8b6ecaa2ae1906
omegamaster86 <masya868686@gmail.com>
2023-06-02 18:05:00 +0900

    カスタムフック化

--- src/component/TodoTemplate/index.jsx
+++ src/component/TodoTemplate/index.jsx
@@ -1,72 +1,15 @@
-import { useState, useMemo } from "react";
 import { InputForm } from "../InputForm/index";
 import { AddTodo } from "../addTodo/index";
 import { TodoList } from "../Todolist/index"
-import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../data";
 import styles from "./style.module.css";
+import { useHooks } from "../../hooks/useHooks";
+
 
 export const TodoTemplate = () => {
-  /* todo list */
-  const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
-  /* add input title */
-  const [addInputValue, setAddInputValue] = useState("");
-  /* todo 採番ID */
-  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
-  /* 検索キーワード */
-  const [searchKeyword, setSearchKeyword] = useState("");
-
-//   const showTodoList = () => {
-//     return (
-//         originTodoList.filter(todo =>
-//         todo.title.includes(searchKeyword))
-//           )};
-
-  const showTodoList = useMemo(() => {
-    return originTodoList.filter((todo) => {
-      // 検索キーワードに部分一致したTodoだけを一覧表示する
-      const regexp = new RegExp("^" + searchKeyword, "i");
-      return todo.title.match(regexp);
-    });
-  }, [originTodoList, searchKeyword]);
-
-//  addInputValueの変更処理
-
-  const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
-
-  const addTodo = (e) => {
-    if (e.key === "Enter" && addInputValue !== "") {
-        const nextUniqueId = uniqueId + 1;
-        const newTodoList = [
-            ...originTodoList,
-            {
-              id: nextUniqueId,
-              title: addInputValue,
-            },
-        ];
-        setOriginTodoList(newTodoList);
-    
-        // 採番IDを更新
-        setUniqueId(nextUniqueId);
-        // todo追加後、入力値をリセット
-        setAddInputValue("");
-      };
-  };
-
-   /**
-   * Todo削除処理
-   * @param { number } targetId
-   * @param { string }targetTitle
-   */
-  const deleteTodo = (targetId, targetTitle) => {
-    if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
-      // 削除するid以外のtodoリストを再編集
-      const newTodoList = originTodoList.filter((todo) => todo.id !== targetId);
-      // todoを削除したtodo listで更新
-      setOriginTodoList(newTodoList);
-    }
-  };
-  // 検索キーワード更新処理
-  const changeSearchKeyword = (e) => setSearchKeyword(e.target.value);
+  const [
+  { showTodoList, searchKeyword, addInputValue },
+  { onChangeAddInputValue, addTodo, deleteTodo, changeSearchKeyword },
+  ] = useHooks();
 
   return (
     <div className={styles.container}>
---
+++ src/hooks/useHooks.jsx
@@ -0,0 +1,66 @@
+import { useState, useMemo } from "react";
+import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../data"
+
+export const useHooks = () => {
+    const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
+  /* add input title */
+  const [addInputValue, setAddInputValue] = useState("");
+  /* todo 採番ID */
+  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
+  /* 検索キーワード */
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const showTodoList = useMemo(() => {
+    return originTodoList.filter((todo) => {
+      // 検索キーワードに部分一致したTodoだけを一覧表示する
+      const regexp = new RegExp("^" + searchKeyword, "i");
+      return todo.title.match(regexp);
+    });
+  }, [originTodoList, searchKeyword]);
+
+//  addInputValueの変更処理
+
+  const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
+
+  const addTodo = (e) => {
+    if (e.key === "Enter" && addInputValue !== "") {
+        const nextUniqueId = uniqueId + 1;
+        const newTodoList = [
+            ...originTodoList,
+            {
+              id: nextUniqueId,
+              title: addInputValue,
+            },
+        ];
+        setOriginTodoList(newTodoList);
+    
+        // 採番IDを更新
+        setUniqueId(nextUniqueId);
+        // todo追加後、入力値をリセット
+        setAddInputValue("");
+      };
+  };
+
+   /**
+   * Todo削除処理
+   * @param { number } targetId
+   * @param { string }targetTitle
+   */
+  const deleteTodo = (targetId, targetTitle) => {
+    if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
+      // 削除するid以外のtodoリストを再編集
+      const newTodoList = originTodoList.filter((todo) => todo.id !== targetId);
+      // todoを削除したtodo listで更新
+      setOriginTodoList(newTodoList);
+    }
+  };
+  // 検索キーワード更新処理
+  const changeSearchKeyword = (e) => setSearchKeyword(e.target.value);
+
+
+  const states = { showTodoList, searchKeyword, addInputValue };
+
+  const actions = { onChangeAddInputValue, addTodo, deleteTodo, changeSearchKeyword };
+  
+  return [states, actions];
+};
