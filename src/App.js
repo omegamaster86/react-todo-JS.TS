826688461adaeb1aec03922547fac483484407cd
omegamaster86 <masya868686@gmail.com>
2023-06-04 18:54:27 +0900

    TodoProvider 設置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { TodoTemplate } from './component/TodoTemplate/index';
+import { TodoProvider } from "../src/context/TodoContext";
 
 const App = () => {
   return (
-    <div className='App'> 
+    <TodoProvider> 
       <TodoTemplate />
-    </div>
+    </TodoProvider>
   );
 };
 
