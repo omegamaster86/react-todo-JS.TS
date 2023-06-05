5ffbc8b2dd492b68203bd0aed2a3e3da3fdc6ebd
omegamaster86 <masya868686@gmail.com>
2023-06-05 19:28:47 +0900

    first commit

---
+++ src/component/TodoTemplate/TodoCreateTemplate/index.jsx
@@ -0,0 +1,41 @@
+ import { useTodoContext } from "../../../context/TodoContext";
+ import { BaseLayout } from "../../BaseLayout/index";
+ import { InputForm } from "../../atoms/InputForm";
+ import { TextArea } from "../../atoms/TextArea";
+ import { CommonButton } from "../../atoms/CommonButton";
+ import { useTodoCreateTemplate } from "./useTopCreateTemplate.js";
+ import styles from "./styles.module.css";
+ 
+
+ export const TodoCreateTemplate = () => {
+   const { addTodo } = useTodoContext();
+   const [
+     { inputTitle, inputContent },
+     { handleChangeTitle, handleChangeContent, handleCreateTodo },
+   ] = useTodoCreateTemplate({ addTodo });
+ 
+   return (
+     <BaseLayout title={"Create Todo"}>
+       <form className={styles.container} onSubmit={handleCreateTodo}>
+         <div className={styles.area}>
+           <InputForm
+             value={inputTitle}
+             placeholder={"Title"}
+             onChange={handleChangeTitle}
+           />
+         </div>
+         <div className={styles.area}>
+           <TextArea
+             value={inputContent}
+             placeholder={"Content"}
+             onChange={handleChangeContent}
+           />
+         </div>
+         <div className={styles.area}>
+           <CommonButton type="submit" label="Create Todo" />
+         </div>
+       </form>
+     </BaseLayout>
+   );
+ };
+ 
