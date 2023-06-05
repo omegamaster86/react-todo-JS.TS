d7bf2e273d144385ce9a626882ce17fcf6fc455d
omegamaster86 <masya868686@gmail.com>
2023-06-05 15:23:41 +0900

    router ファイル、フォルダー 設置

---
+++ src/routers/index.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import { NAVIGATION_LIST } from "../constants/navigations";
+import { TodoPage } from "../pages/todo";
+import { TodoDetailPage } from "../pages/detail.jsx";
+import { TodoCreatePage } from "../pages/create";
+import { TodoEditPage } from "../pages/edit";
+import React from "react";
+
+export const router = createBrowserRouter([
+  {
+    path: NAVIGATION_LIST.TOP,
+    element: <TodoPage />,
+  },
+  {
+    path: NAVIGATION_LIST.DETAIL,
+    element: <TodoDetailPage />,
+  },
+  {
+    path: NAVIGATION_LIST.CREATE,
+    element: <TodoCreatePage />,
+  },
+  {
+    path: NAVIGATION_LIST.EDIT,
+    element: <TodoEditPage />,
+  },
+]);
